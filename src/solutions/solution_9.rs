bc9da770f25827a1ab72a7f10a8e3304ea4cf673
StanMarek <stanislawmarek21@gmail.com>
2023-12-09 15:49:54 +0100

    Day 9 part 2

--- src/solutions/solution_9.rs
+++ src/solutions/solution_9.rs
@@ -2,6 +2,7 @@ pub fn solve() {
     println!("Day 9");
 
     sub_solution_1();
+    sub_solution_2();
 }
 
 fn sub_solution_1() {
@@ -18,12 +19,12 @@ fn sub_solution_1() {
         let mut accumulator = Vec::new();
 
         solve_series(series.clone(), &mut accumulator);
-        solve_accumulator(&mut accumulator);
+        extrapolate(&mut accumulator, 1);
 
         output += accumulator.last().unwrap().last().unwrap();
     }
 
-    println!("\tSub solution 2: {output}");
+    println!("\tSub solution 1: {output}");
 }
 
 fn solve_series(main_series: Vec<i64>, accumulator: &mut Vec<Vec<i64>>) -> () {
@@ -40,11 +41,40 @@ fn solve_series(main_series: Vec<i64>, accumulator: &mut Vec<Vec<i64>>) -> () {
     accumulator.push(main_series);
 }
 
-fn solve_accumulator(accumulator: &mut Vec<Vec<i64>>) {
+fn extrapolate(accumulator: &mut Vec<Vec<i64>>, extrapolation: i8) {
     for index in 0..accumulator.len() - 1 {
-        let bottom = *accumulator.get(index).unwrap().last().unwrap();
-        let above = *accumulator.get(index + 1).unwrap().last().unwrap();
+        if extrapolation == 1 {
+            let bottom = *accumulator.get(index).unwrap().last().unwrap();
+            let above = *accumulator.get(index + 1).unwrap().last().unwrap();
 
-        accumulator.get_mut(index + 1).unwrap().push(above + bottom);
+            accumulator.get_mut(index + 1).unwrap().push(above + bottom);
+        }
+        if extrapolation == -1 {
+            let bottom = *accumulator.get(index).unwrap().last().unwrap();
+            let above = *accumulator.get(index + 1).unwrap().first().unwrap();
+
+            accumulator.get_mut(index + 1).unwrap().push(above - bottom);
+        }
     }
+}
+
+fn sub_solution_2() {
+    let input = include_str!("../inputs/day9/input.txt").lines();
+
+    let mut output = 0;
+
+    for line in input {
+        let series = line
+            .split_whitespace()
+            .map(|number| number.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
+
+        let mut accumulator = Vec::new();
+
+        solve_series(series.clone(), &mut accumulator);
+        extrapolate(&mut accumulator, -1);
+        output += accumulator.last().unwrap().last().unwrap();
+    }
+
+    println!("\tSub solution 2: {output}");
 }
